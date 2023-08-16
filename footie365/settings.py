a8c9e420037f56be851a5df08d98c56589f428c0
chancandan <daniellyons@live.ie>
2023-08-16 23:30:11 +0000

    Create and develop models and admin for site

--- footie365/settings.py
+++ footie365/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-chancandan-portfoliopro-k67yviaamsh.ws-eu103.gitpod.io', 'footie365-3519eaeedcde.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-chancandan-portfoliopro-k67yviaamsh.ws-eu104.gitpod.io', 'footie365-3519eaeedcde.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
