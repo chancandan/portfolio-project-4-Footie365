1997be4a71da7769b316ce5b45f4b684d36d45f1
chancandan <daniellyons@live.ie>
2023-08-27 09:33:54 +0000

    Revert back to last setting on site

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('admin/', admin.site.urls),
 ]
