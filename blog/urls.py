c65413279b6e4eeceb72f7ec6933662831f1d79a
chancandan <daniellyons@live.ie>
2024-02-19 11:52:28 +0000

    Update urls.py to include new views created

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
-from .views import PostCreate
+from .views import PostCreate, PostList, PostDetail, PostLike, PostUpdate, PostDelete
 
 urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('post/create/', PostCreate.as_view(), name="post_create"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('post/<slug:slug>/update/', PostUpdate.as_view(), name='post_update'),
+    path('post/<slug:slug>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
